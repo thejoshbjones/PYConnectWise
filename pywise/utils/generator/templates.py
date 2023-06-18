5927387afbca0239024604d7b96e7c7f40661976
Bailey Eaton <xYoshify@gmail.com>
2023-06-18 22:01:27 +1000

    updated templates, added client comments

--- pywise/utils/generator/templates.py
+++ pywise/utils/generator/templates.py
@@ -1,7 +1,7 @@
 from jinja2 import Template
 
 endpoint_template = Template(
-    """from pywise.models.base.message_model import GenericMessageModel
+"""from pywise.models.base.message_model import GenericMessageModel
 from pywise.endpoints.base.connectwise_endpoint import ConnectWiseEndpoint
 from pywise.responses.paginated_response import PaginatedResponse
 {%- if additional_imports is defined %}
@@ -55,7 +55,7 @@ class {{ endpoint_class }}(ConnectWiseEndpoint):
 )
 
 top_level_endpoint_template = Template(
-    """from pywise.endpoints.base.connectwise_top_level_endpoint import ConnectWiseEndpoint
+"""from pywise.endpoints.base.connectwise_top_level_endpoint import ConnectWiseEndpoint
 {%- if additional_imports is defined %}
 {%- for additional_import in additional_imports %}
 {{ additional_import }}
@@ -76,7 +76,7 @@ class {{ endpoint_class }}(ConnectWiseEndpoint):
 )
 
 model_template_old = Template(
-    """from pywise.models.base.connectwise_model import ConnectWiseModel
+"""from pywise.models.base.connectwise_model import ConnectWiseModel
 {%- if imports is defined %}
 {%- for import in imports %}
 {{ import }}
@@ -95,9 +95,9 @@ class {{ model_class }}():
 )
 
 model_template = Template(
-    """from __future__ import annotations
+"""from __future__ import annotations
 from typing import Any
-from pywise.models.utils.naming import to_camel_case
+from pywise.utils.naming import to_camel_case
 from pywise.models.base.connectwise_model import ConnectWiseModel
 
 {%- if imports is defined %}
@@ -128,9 +128,7 @@ class {{ model_class }}(ConnectWiseModel):
 )
 
 client_template = Template(
-    """
-import os
-import base64
+"""import base64
 import requests
 {%- if imports is defined %}
 {%- for import in imports %}
@@ -139,41 +137,75 @@ import requests
 {%- endif %}
 
 class ConnectWiseManageAPIClient:
+    \"""
+    ConnectWise Manage API client. Handles the connection to the ConnectWise Manage API
+    and the configuration of all the available endpoints.
+    \"""
     def __init__(
         self,
         company_name,
-        company_url,
+        manage_url,
         client_id,
         public_key,
         private_key,
         codebase=None,
     ):
+        \"""
+        Initializes the client with the given credentials and optionally a specific codebase.
+        If no codebase is given, it tries to get it from the API.
+
+        Parameters:
+            company_name (str): Name of your company.
+            manage_url (str): URL of your ConnectWise Manage instance.
+            client_id (str): Your ConnectWise Manage API Client ID.
+            public_key (str): Your ConnectWise Manage API Public key.
+            private_key (str): Your ConnectWise Manage API Private key.
+            codebase (str, optional): Your ConnectWise Manage Codebase. Defaults to None.
+        \"""
         self.client_id = client_id
         self.company_name = company_name
-        self.company_url = company_url
+        self.manage_url = manage_url
         self.public_key = public_key
         self.private_key = private_key
         self.codebase = codebase
 
+        # Retrieve codebase from the API if not provided
         if not codebase:
             self.codebase = self.__try_get_codebase_from_api(
-                company_url=company_url,
+                manage_url=manage_url,
                 company_name=company_name,
                 headers=self.get_headers(),
             )
 
-        ## ENDPOINT REGISTRATION BELOW ##
+        # Initializing endpoints
         {%- for endpoint in endpoints %}
         self.{{ endpoint.field_name }} = {{ endpoint.class_name }}(self)
         {%- endfor %}
 
-    def get_url(self):
-        return f"https://{self.company_url}/{self.codebase.strip('/')}/apis/3.0"
+    def get_url(self) -> str:
+        \"""
+        Generates and returns the URL for the ConnectWise Manage API endpoints based on the company url and codebase.
 
-    def __try_get_codebase_from_api(self, company_url, company_name, headers):
+        Returns:
+            str: API URL.
+        \"""
+        return f"https://{self.manage_url}/{self.codebase.strip('/')}/apis/3.0"
+
+    def __try_get_codebase_from_api(self, manage_url: str, company_name: str, headers: dict[str, str]) -> str:
+        \"""
+        Tries to retrieve the codebase from the API using the provided company url, company name and headers.
+
+        Parameters:
+            company_url (str): URL of the company.
+            company_name (str): Name of the company.
+            headers (dict[str, str]): Headers to be sent in the request.
+
+        Returns:
+            str: Codebase string or None if an error occurs.
+        \"""
         result = ""
         try:
-            url = f"https://{company_url}/login/companyinfo/{company_name}"
+            url = f"https://{manage_url}/login/companyinfo/{company_name}"
             result = (
                 requests.request("GET", url, headers=headers).json().get("Codebase")
             )
@@ -181,7 +213,13 @@ class ConnectWiseManageAPIClient:
             result = None
         return result
 
-    def __get_auth_string(self):
+    def __get_auth_string(self) -> str:
+        \"""
+        Creates and returns the base64 encoded authorization string required for API requests.
+
+        Returns:
+            str: Base64 encoded authorization string.
+        \"""
         return "Basic " + base64.b64encode(
             bytes(
                 f"{self.company_name}+{self.public_key}:{self.private_key}",
@@ -189,7 +227,13 @@ class ConnectWiseManageAPIClient:
             )
         ).decode("ascii")
 
-    def get_headers(self):
+    def get_headers(self) -> dict[str, str]:
+        \"""
+        Generates and returns the headers required for making API requests.
+
+        Returns:
+            dict[str, str]: Dictionary of headers including Content-Type, Client ID, and Authorization.
+        \"""
         headers = {
             "Content-Type": "application/json",
             "clientId": self.client_id,
